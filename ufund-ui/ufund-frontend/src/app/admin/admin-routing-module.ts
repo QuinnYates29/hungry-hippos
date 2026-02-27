de5ad81a35006baeb47718bcb5ab8437a6193178
QuinnYates29 <quinnyates3229@gmail.com>
2026-02-26 22:02:29 -0500

    Added route for app-routing to admin-module

--- ufund-ui/ufund-frontend/src/app/admin/admin-routing-module.ts
+++ ufund-ui/ufund-frontend/src/app/admin/admin-routing-module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
 
-
+const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AdminRoutingModule { }
