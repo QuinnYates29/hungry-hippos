a846f3a4c9c596d632ad3b174e8b3ef71919dd85
QuinnYates29 <quinnyates3229@gmail.com>
2026-03-02 12:04:18 -0500

    Updated routing in admin-routing-module

--- ufund-ui/ufund-frontend/src/app/admin/admin-routing-module.ts
+++ ufund-ui/ufund-frontend/src/app/admin/admin-routing-module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Login } from '../login/login';
+//import { LoginComponent } from '../login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 const routes: Routes = [
-  { path: 'login', component: Login },
+  //{ path: 'login', component: LoginComponent },
   { path: 'dashboard', component: Dashboard}
 ];
 
